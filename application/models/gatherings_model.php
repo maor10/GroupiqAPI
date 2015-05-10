32cfca21f9b18893d6ed14665d164bec44e8b100
Guest <guest-tmMucL@meet.mit.edu>
2015-05-10 18:35:27 +0300

    yyy

--- application/models/gatherings_model.php
+++ application/models/gatherings_model.php
@@ -15,13 +15,14 @@ class Gatherings_model extends CI_Model {
     function createNewGathering($name, $startDate, $public, $latitude, $longitude) {
         $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         $password = substr( str_shuffle( $chars ), 0, 5 );
+
         $data = array('creatorid'=>  $this->session->userdata("UserID"), 'name' => $name, 'public'=>$public, 'password' => $password);
         $this->db->insert('gatherings', $data);
 
         if ($this->db->_error_message()) {
-            echo $this->db->error_message();
+            return $this->db->error_message();
         } else {
-            echo "0";
+            return $this->db->insert_id();
         }
     }
 
